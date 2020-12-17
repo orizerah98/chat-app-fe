82b24b5e21556d314c76136b255c8df92317252f
Ori Zerah <orizerah@Oris-MacBook-Pro.local>
2020-12-17 10:56:04 +0200

    Renamed MenuItem to ChatMegst

---
+++ src/components/chats/ChatMenuItem.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+
+export default function ChatMenuItem(props: any) {
+  const chat = props;
+
+  return (
+    <ListItem button key={props.name} onClick={props.setChat}>
+      <ListItemIcon>
+        <Avatar src={props.iconUrl} />
+      </ListItemIcon>
+      <ListItemText primary={props.name} />
+    </ListItem>
+  );
+}
